8dfa12f2f7ecb17796627ee6667f2473c2f601b9
ZhouSicheng-2011 <ZSCinYBSZ2023@outlook.com>
2025-10-04 22:42:01 +0800

    Update BaseFileViewer.py

--- MyZip/BaseFileViewer.py
+++ MyZip/BaseFileViewer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import ttkthemes
 
 import abc
@@ -35,7 +36,7 @@ class BaseFileViewer(abc.ABC):
         ##
         ##
         #创建表格
-        self.file_table = ttk.Treeview(self.frame, columns=('文件名', '压缩前大小', '压缩后大小',\
+        self.file_table = ttk.Treeview(self.frame, columns=('文件名/目录名', '压缩前大小', '压缩后大小',\
                                                             '压缩率', '修改日期', 'CRC32校验值',\
                                                             '压缩方式/算法'),\
                                                             show='headings')
@@ -46,6 +47,8 @@ class BaseFileViewer(abc.ABC):
         self.file_table.column('修改日期', width=180, anchor='center')
         self.file_table.column('CRC32校验值', width=100, anchor='center')
         self.file_table.column('压缩方式/算法', width=100, anchor='center')
+
+        self.file_table.bind('Double-1', self.next)
         #创建滚动条
         self.scr_vertical = ttk.Scrollbar(self.frame, orient='vertical', command=self.file_table.yview)
         self.scr_horizontal = ttk.Scrollbar(self.frame, orient='horizontal', command=self.file_table.xview)
@@ -112,7 +115,11 @@ class BaseFileViewer(abc.ABC):
         self.now_dir.set(now_dir)
 
     def last(self):
-        pass
+        if self.now_dir.get() == '':
+            messagebox.showerror('错误', '压缩文件当前显示目录没有上一级目录了!')
+        else:
+            target_dir = '/'.join(self.now_dir.get().split('/')[:-1])
+            self.get_fd_and_info_and_update(target_dir)
 
     def find_fd_in_one_dir(self, target_dir:str):
         '''寻找在压缩文件里在同一个目录下的文件和目录'''
@@ -120,3 +127,30 @@ class BaseFileViewer(abc.ABC):
         files = [k for k in self.fn_list if k.startswith(target_dir) and k.count('/')==num]
         dirs = [p for p in self.dn_set if p.startswith(target_dir) and p.count('/')==num+1]
         return tuple(files), tuple(dirs)
+    
+    def get_fd_and_info_and_update(self, path:str):
+        self.status_var.set('正在加载文件与信息...')
+        files, dirs = self.find_fd_in_one_dir(path)
+        fwi = [(f,)+self.fp_info.get(f, tuple()) for f in files] #fwi是file with information的缩写
+        dwi = [(d,)+self.fp_info.get(d, tuple()) for d in dirs] #dwi是directory with information的缩写
+
+        self.show_update(fwi, dwi, path)
+
+    def next(self, event):
+        w = event.widget
+        try:
+            item_id = w.identify("item", event.x, event.y)
+            filename = w.item(item_id, "values")[0]
+        except:
+            return
+        
+        if filename.endswith('/'):
+            fn = f"{self.now_dir.get()}/{filename}" if self.now_dir.get() else filename
+            self.get_fd_and_info_and_update(fn)
+        else:
+            fn = f"{self.now_dir.get()}/{filename}" if self.now_dir.get() else filename
+            self.extract_file(fn)
+
+    @abc.abstractmethod
+    def extract_file(self, file:str):
+        pass
